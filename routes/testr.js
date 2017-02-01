c559b9c8b915ad57a1cf6e68cb4a5e3e1c803e34
Temp <Temp>
2017-02-01 21:29:08 +0200

    Lee

--- routes/testr.js
+++ routes/testr.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('/public/htmls/test');
+  res.render('./public/htmls/test');
 });
 
 module.exports = router;
