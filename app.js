e6b1b47bb44b2bcaa293211cc1eb64c1aa18102a
Josefi <Josefi@DESKTOP-D37K14T>
2017-02-01 20:36:28 +0200

    More mongoose

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+//var Promise = require('bluebird');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var database = require('./config/database');
 var mongoose = require('mongoose');
 
 var index = require('./routes/index');
@@ -11,7 +13,8 @@ var users = require('./routes/users');
 
 var app = express();
 
-mongoose.connect(database.url);
+mongoose.Promise = require('bluebird');
+mongoose.createConnection(database.url);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,7 +31,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-mongoose.connect(database.url);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,2 @@
+/// <reference path="globals/mongoose/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
