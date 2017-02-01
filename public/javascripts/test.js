db645b3079c1034a03d41dda10a86232ee6db2ac
Temp <Temp>
2017-02-01 22:53:58 +0200

    dd

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,4 +1,7 @@
 var app = angular.module('Incognitive', []);
 app.controller('ctr', function($scope) {
     $scope.imgInput="Enter URL";
+    $scope.doMagic=function() {
+        $scope.imgInput = "Clicked";
+    };
 });
