85444d0160c9c4f4bd5c234ac0cfdb02b94a4faa
Josefi <Josefi@DESKTOP-D37K14T>
2017-02-01 20:09:41 +0200

    Added mongoose

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+	// the database url to connect
+	url : 'mongodb://mongo11:qmictnkyO6CJOpdn82nqO13eZeJMSe1pSENZxtBYKjb5fmYknHwfcrRraE4i8k6XT09xiHN5DznaKgvgieZFBA==@mongo11.documents.azure.com:10250/?ssl=true'
+}
