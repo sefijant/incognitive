2d4c8ed5cb32fccb209c9107153f51effe8f2aa9
Josefi <Josefi@DESKTOP-D37K14T>
2017-02-01 21:19:50 +0200

    Reverse

--- app.js
+++ app.js
@@ -29,15 +29,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', index);
-//app.use('/users', users);
-//app.use('/test', test);
-
-require('.\routes\router.js')(app);
-
-// listen (start app with node server.js) ======================================
-app.listen(port);
-console.log("App listening on port " + port);
+app.use('/', index);
+app.use('/users', users);
+app.use('/test', test);
 
 
 // catch 404 and forward to error handler
