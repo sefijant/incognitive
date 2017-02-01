49257913d70ddefdd2220d251b48f3b648762cdf
Temp <Temp>
2017-02-02 00:44:30 +0200

    meh

--- routes/testr.js
+++ routes/testr.js
@@ -7,7 +7,19 @@ router.get('/', function(req, res, next) {
   res.render('test.html');
 });
 router.post('/check', function(req, res, next) {
-    res.send("E");
+    var options = {  
+        url: 'https://westus.api.cognitive.microsoft.com/face/v1.0/detect',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': '580eb3f9f7f64f9aaf4afb69c25ffd40'
+        },
+        json: {'url':'http://cdn.thefederalist.com/wp-content/uploads/2015/06/Trump-Fox-998x624.jpg'}
+};
+
+request(options, function(err, res, body) {  
+    res.send(body);
+});
     // var pid ="";
     // request({
     //     url: "https://westus.api.cognitive.microsoft.com/face/v1.0/detect",
