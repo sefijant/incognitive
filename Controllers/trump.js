9d798aedd1a427baad7404a734e84e87d8ec24aa
Temp <Temp>
2017-02-02 02:44:32 +0200

    sd

--- Controllers/trump.js
+++ Controllers/trump.js
@@ -1,13 +1,15 @@
 var request = require('request');
 
 exports.apiPOST = function(req, res) {
+    var che = req;
   request.post({
     headers: {'Content-Type': "application/json", 'Ocp-Apim-Subscription-Key': "580eb3f9f7f64f9aaf4afb69c25ffd40"},
     url:     'https://westus.api.cognitive.microsoft.com/face/v1.0/detect',
     json:   true,
     body:    {"url":"http://cdn.thefederalist.com/wp-content/uploads/2015/06/Trump-Fox-998x624.jpg"}
     }, function(error, response, body){
-        res.send(body[0].faceId);
+        //res.send(body[0].faceId);
+        res.send(che);
     });
 };
 
