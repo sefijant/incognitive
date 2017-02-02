c9c05df322328b429d7b747ebbf1f3a0e200e7fe
Josefi <Josefi@DESKTOP-D37K14T>
2017-02-02 03:29:46 +0200

    dd

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/process', process);
+app.use('/users', users);
 app.use('/trump', test);
 
 
