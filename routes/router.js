b50fc0a18d1ba98060303491059e2ff2f3915426
Josefi <Josefi@DESKTOP-D37K14T>
2017-02-02 03:37:43 +0200

    Halevai

--- routes/router.js
+++ routes/router.js
@@ -1,25 +1,27 @@
-﻿angular.module('route', ['ngRoute']).config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-
-        // route for the home page
-        .when('/', {
-            templateUrl: 'views/home.html',
-            controller: 'mainCtrl'
-        })
-
-        .when('/process', {
-            template: 'views/process.html',
-            controller: 'processCtrl'
-        })
-        .when('/err', {
-            template: 'views/error.html',
-            controller: 'errCtrl'
-        })
-        .otherwise({ redirectTo: '/' });
-
-    
-});
+﻿
 
 function MainCtrl($scope) {
     $scope.test = "123";
 }
+
+var mainApp = angular.module("mainApp", ['ngRoute']);
+
+mainApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+
+        when('/', {
+            templateUrl: 'views/home.html', controller: 'mainCtrl'
+        }).
+        when('/process', {
+            templateUrl: 'views/process.html', controller: 'processCtrl'
+        }).
+
+        when('/err', {
+            templateUrl: 'views/error.html', controller: 'errCtrl'
+        }).
+
+        otherwise({
+            redirectTo: '/addStudent'
+        });
+
+}]);
