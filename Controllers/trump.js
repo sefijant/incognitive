214cd063993a0907db2513c1708417606398c588
Temp <Temp>
2017-02-02 02:40:50 +0200

    fd

--- Controllers/trump.js
+++ Controllers/trump.js
@@ -5,7 +5,7 @@ exports.apiPOST = function(req, res) {
     headers: {'Content-Type': "application/json", 'Ocp-Apim-Subscription-Key': "580eb3f9f7f64f9aaf4afb69c25ffd40"},
     url:     'https://westus.api.cognitive.microsoft.com/face/v1.0/detect',
     json:   true,
-    body:    {"url":req.ur}
+    body:    {"url":req[0].ur}
     }, function(error, response, body){
         res.send(body[0].faceId);
     });
