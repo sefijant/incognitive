c6adb6d09708489f7c0d0560e34a2a1078d361bf
Temp <Temp>
2017-02-02 04:44:37 +0200

    as

--- Controllers/trump.js
+++ Controllers/trump.js
@@ -12,9 +12,9 @@ app.use(bodyParser.json());
 exports.apiPOST = function(req, res) {
   request.post({
     headers: {'Content-Type': "application/json", 'Ocp-Apim-Subscription-Key': "580eb3f9f7f64f9aaf4afb69c25ffd40"},
-    url:     req.body.ur,
+    url:     'https://westus.api.cognitive.microsoft.com/face/v1.0/detect',
     json:   true,
-    body:    {"url":"http://cdn.thefederalist.com/wp-content/uploads/2015/06/Trump-Fox-998x624.jpg"}
+    body:    {'url' : req.body.ur}
     }, function(error, response, body){
         res.send(body[0].faceId);
         //res.send(req.body.ur);
