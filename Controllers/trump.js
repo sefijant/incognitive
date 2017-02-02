585c40d7eeaf86d3dd27d9c163ca20184eda3188
Temp <Temp>
2017-02-02 02:07:53 +0200

    god bless america

--- Controllers/trump.js
+++ Controllers/trump.js
@@ -4,6 +4,7 @@ exports.apiPOST = function(req, res) {
   request.post({
     headers: {'Content-Type': "application/json", 'Ocp-Apim-Subscription-Key': "580eb3f9f7f64f9aaf4afb69c25ffd40"},
     url:     'https://westus.api.cognitive.microsoft.com/face/v1.0/detect',
+    json:   true,
     body:    {"url":"http://cdn.thefederalist.com/wp-content/uploads/2015/06/Trump-Fox-998x624.jpg"}
     }, function(error, response, body){
         console.log(body);
