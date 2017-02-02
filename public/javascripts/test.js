3957764aea9918df95054a0007f6e0b6512c65b5
Temp <Temp>
2017-02-02 05:00:49 +0200

    0

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -15,7 +15,7 @@ app.controller('ctr', function($scope, $http) {
                 data: { 'bdy' : response }
             })
             .then(function(dt) {
-                $scope.result = dt.data.faceid;
+                $scope.result = dt.data[0];
             }, 
             function(dt) { // optional
                 $scope.result = "err2";
