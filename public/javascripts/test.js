5b0a94b378207867ef6c4c575b952bd2264a30cd
Temp <Temp>
2017-02-02 03:03:25 +0200

    fg

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -2,12 +2,11 @@ var app = angular.module('Incognitive', []);
 app.controller('ctr', function($scope, $http) {
     $scope.result = "";
     $scope.imgInput="Enter URL";
-    $scope.jsonush = JSON.parse({"ur":$scope.imgInput});
     $scope.doMagic=function() {
         var req = {
             method: 'POST',
             url: 'http://incognitive.azurewebsites.net/trump/detect',
-            data: jsonush
+            data: {'ur':$scope.imgInput}
         }
         $http(req).then(function(bd){
             $scope.result = bd.data;
